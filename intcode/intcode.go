8559de4027eb281b8be84fc656eac6c44d4dd0db
nk <nkibbey@terpmail.umd.edu>
2019-12-17 22:37:12 -0500

    add basic import

---
+++ intcode/intcode.go
@@ -0,0 +1,7 @@
+package intcode
+
+func Add(a, b int) int{
+	return a+b
+}
+
+
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+import "github.com/nkibbey/aoc/intcode"
+
+func main() {
+
+	fmt.Println("hi")
+	fmt.Printf("1 + 3 = %d\n", intcode.Add(1,3))
+}
