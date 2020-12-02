8ff6a94d5b44bb1a4812c679dbfc1644df344f06
nk <nkibbey@terpmail.umd.edu>
2020-12-01 21:45:16 -0500

    day1 part2 success

--- 2020/day1/main.go
+++ 2020/day1/main.go
@@ -5,10 +5,26 @@ import (
 	"fmt"
 )
 
+// Find3EltSum finds 3 elts in list that sum to given sum number
+func Find3EltSum(elts []int, sum int) (int, int, int) {
+	for i := range elts {
+		for j := i + 1; j < len(elts); j++ {
+			for k := j + 1; k < len(elts); k++ {
+				if elts[i]+elts[j]+elts[k] == sum {
+					return elts[i], elts[j], elts[k]
+				}
+			}
+
+		}
+	}
+	return 0, 0, 0
+}
+
+// Find2EltSum finds 2 elts in list that sum to given sum number
 func Find2EltSum(elts []int, sum int) (int, int) {
 	for i := range elts {
 		for j := i + 1; j < len(elts); j++ {
-			if elts[i] + elts[j] == sum {
+			if elts[i]+elts[j] == sum {
 				return elts[i], elts[j]
 			}
 		}
@@ -19,4 +35,7 @@ func Find2EltSum(elts []int, sum int) (int, int) {
 func main() {
 	a, b := Find2EltSum(utils.GetIntSlice("resources/day1.csv"), 2020)
 	fmt.Println(a * b)
+
+	a, b, c := Find3EltSum(utils.GetIntSlice("resources/day1.csv"), 2020)
+	fmt.Println(a * b * c)
 }
