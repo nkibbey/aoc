3d9f1c8df11b1f77dbac13ccbc1dbf5f692b8103
nk <nkibbey@terpmail.umd.edu>
2020-12-01 21:37:00 -0500

    day1 part1 success

--- 2020/day1/main.go
+++ 2020/day1/main.go
@@ -6,6 +6,13 @@ import (
 )
 
 func Find2EltSum(elts []int, sum int) (int, int) {
+	for i := range elts {
+		for j := i + 1; j < len(elts); j++ {
+			if elts[i] + elts[j] == sum {
+				return elts[i], elts[j]
+			}
+		}
+	}
 	return 0, 0
 }
 
