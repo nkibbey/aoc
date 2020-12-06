0f13ad6697282f2e59c19b6db4dab8c2179dd014
nk <nkibbey@terpmail.umd.edu>
2020-12-06 01:42:04 -0500

    day 6 part 2 success

--- 2020/day6/main.go
+++ 2020/day6/main.go
@@ -15,23 +15,19 @@ func getSlice(fn string) []string {
 	return strings.Split(string(input), "\n")
 }
 
-// func addUniq(group, indiv string) string{
-// 	res 
-// 	for i := 0; i < len(group); i++ {
-// 		uniq := true
-// 		for j := 0; j<len(indiv); j++ {
-// 			if indiv[j] == indiv[i] {
-// 				uniq = false
-// 				continue
-// 			}
-// 		}
-// 		if uniq {
+func makeUnanimous(group, indiv string, first bool) string{
+	res := ""
+	if first {
+		return indiv
+	}
+	for i := 0; i<len(indiv); i++ {
+		if strings.Contains(group, string(indiv[i])) {
+			res += string(indiv[i])
+		}
+	}
 
-// 		}
-// 	}
-
-// 	return ""
-// }
+	return res
+}
 
 func uniqs(ans string) int {
 	u := ""
@@ -43,15 +39,33 @@ func uniqs(ans string) int {
 	return len(u)
 }
 
+// func answerSums(answers []string) int {
+// 	sum := 0
+// 	curr := ""
+// 	for _, answer := range answers {
+// 		if answer == "" {
+// 			sum+=uniqs(curr)
+// 			curr = ""
+// 		} else {
+// 			curr += answer
+// 		}
+// 	}
+// 	return sum
+// }
+
 func answerSums(answers []string) int {
 	sum := 0
 	curr := ""
+	first := true
 	for _, answer := range answers {
 		if answer == "" {
-			sum+=uniqs(curr)
+			fmt.Println(curr)
+			sum+=len(curr)
 			curr = ""
+			first = true
 		} else {
-			curr += answer
+			curr = makeUnanimous(curr, answer, first)
+			first = false
 		}
 	}
 	return sum
