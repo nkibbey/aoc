12b2563b304f0a24b88f1edd85142fa8e140dabf
nk <nkibbey@terpmail.umd.edu>
2020-12-12 11:33:23 -0500

    day 12 part 2 success

--- 2020/day12/main.go
+++ 2020/day12/main.go
@@ -6,72 +6,64 @@ import (
 	"strconv"
 )
 
-func move(x, y int, dir string, dist int) (int, int) {
-	if dir == "E" { 
-		return x + dist, y
+func move(x, y, x2, y2 int, dir string, dist int) (int, int, int, int) {
+	if dir == "E" {
+		return x, y, (x2 + dist), y2
 	} else if dir == "N" {
-		return x, y + dist
-	} else if dir == "W" { 
-		return x - dist, y
-	} else { // south
-		return x, y - dist
+		return x, y, x2, (y2 + dist)
+	} else if dir == "W" {
+		return x, y, (x2 - dist), y2
+	} else if dir == "S" {
+		return x, y, x2, (y2 - dist)
+	} else {
+		x += x2 * dist
+		y += y2 * dist
+		return x, y, x2, y2
 	}
 }
 
-func parseAction(a, pdir string) (dir string, dist int, change bool) {
-	dir, dist, change = pdir, 0, false
+func parseAction(a string, x, y int) (dir string, dist int, nx int, ny int) {
+	dir, dist, nx, ny = "N", 0, x, y
 	if len(a) < 1 {
 		fmt.Println("aaaaa")
 		return
 	}
 	d := a[:1]
 	amount, _ := strconv.Atoi(a[1:])
-	
-	if d == "E" || d == "N" || d == "W" || d == "S" {
+
+	if d == "E" || d == "N" || d == "W" || d == "S" || d == "F" {
 		dist = amount
 		dir = d
 		return
 	}
-	if d == "F" {
-		dir = pdir
-		dist = amount
-		return
-	}
-
-
-	enws := []string{"E", "N", "W", "S"}
-	i:= 0
-	for j,elt := range enws {
-		if elt == pdir {
-			i = j
-			break
+	amount = amount / 90 % 4
+	dist = 0
+	if d == "L" {
+		if amount == 1 {
+			amount = 3
+		} else if amount == 3 {
+			amount = 1
 		}
 	}
-	if d == "L" {
-		amount = amount/90
-		i = (i + amount) % len(enws)
-	} else if d == "R" {
-		i = ((16+i)-(amount/90)) % 4 //  -2 mod 4 = -2 so I bumped the minuend
+
+	for i := 0; i < amount; i++ {
+		nx = y
+		ny = x * -1
+		x, y = nx, ny
 	}
-	dir = enws[i]
-	dist = 0
-	change = true
 	return
+
 }
 
-
-func sail(actions[]string) {
-	x, y, face := 0,0,"E"
+func sail(actions []string) {
+	dir, dist, x, y, x2, y2 := "F", 0, 0, 0, 10, 1
 	for _, action := range actions {
-		dir, dist, change := parseAction(action, face) 
-		x,y = move(x,y,dir,dist) 
-		if change{
-			face = dir
-		}
+		dir, dist, x2, y2 = parseAction(action, x2, y2)
+		x, y, x2, y2 = move(x, y, x2, y2, dir, dist)
+		// fmt.Println("wx: ", x2, ", wy: ", y2)
 	}
 	fmt.Println("x: ", x, ", y: ", y)
 }
-
 
 func main() {
 	sail(utils.FileToStringsBy("samp.txt", "\n"))
