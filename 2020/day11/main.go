36180e85b3a0787086938adf914e2a59fa615539
nk <nkibbey@terpmail.umd.edu>
2020-12-11 22:18:29 -0500

    day 11 part 2 succes
    it aint pretty tho

--- 2020/day11/main.go
+++ 2020/day11/main.go
@@ -1,20 +1,100 @@
 package main
 
 import (
-	"strings"
 	"aoc/utils"
 	"fmt"
+	"strings"
 )
 
-// func march(i, j int, seats[]string, dir string) int {
-// 	if dir == "L" {
-// 		for j > 0; j-- {
-// 			c := string(seats[i][j])
-// 			if c:
-// 		}
-// 	}
-// 	return 0
-// }
+func march(i, j int, seats []string, dir string) int {
+	if dir == "L" {
+		for j > 0 {
+			j--
+			c := string(seats[i][j])
+			if c == "#" {
+				return 1
+			} else if c == "L" {
+				return 0
+			}
+		}
+	} else if dir == "UL" {
+		for j > 0 && i > 0 {
+			i--
+			j--
+			c := string(seats[i][j])
+			if c == "#" {
+				return 1
+			} else if c == "L" {
+				return 0
+			}
+		}
+	} else if dir == "U" {
+		for i > 0 {
+			i--
+			c := string(seats[i][j])
+			if c == "#" {
+				return 1
+			} else if c == "L" {
+				return 0
+			}
+		}
+	} else if dir == "UR" {
+		for j < len(seats[0])-1 && i > 0 {
+			i--
+			j++
+			c := string(seats[i][j])
+			if c == "#" {
+				return 1
+			} else if c == "L" {
+				return 0
+			}
+		}
+	} else if dir == "R" {
+		for j < len(seats[0])-1 {
+			j++
+			c := string(seats[i][j])
+			if c == "#" {
+				return 1
+			} else if c == "L" {
+				return 0
+			}
+		}
+	} else if dir == "DR" {
+		for j < len(seats[0])-1 && i < len(seats)-1 {
+			i++
+			j++
+			c := string(seats[i][j])
+			if c == "#" {
+				return 1
+			} else if c == "L" {
+				return 0
+			}
+		}
+	} else if dir == "D" {
+		for i < len(seats)-1 {
+			i++
+			c := string(seats[i][j])
+			if c == "#" {
+				return 1
+			} else if c == "L" {
+				return 0
+			}
+		}
+	} else if dir == "DL" {
+		for j > 0 && i < len(seats) -1 {
+			i++
+			j--
+			c := string(seats[i][j])
+			if c == "#" {
+				return 1
+			} else if c == "L" {
+				return 0
+			}
+		}
+	} 
+
+	return 0
+}
 
 func applyRules(seats []string) []string {
 	ns := []string{}
@@ -26,44 +106,22 @@ func applyRules(seats []string) []string {
 				nl += "."
 				continue
 			}
-			target := "#"
 			fellas := 0
 			if c != "#" && c != "L" {
 				fmt.Println("ahhhh")
 				return []string{}
 			}
-			// up down left right
-			if j > 0 && string(line[j-1]) == target {
-				fellas++
-			}
-			if j < len(line)-1 && string(line[j+1]) == target {
-				fellas++
-			}
-			if i > 0 && string(seats[i-1][j]) == target {
-				fellas++
-			}
-			if i < len(seats)-1 && string(seats[i+1][j]) == target {
-				fellas++
-			}
-
-			//upleft
-			if j > 0 && i > 0 && string(seats[i-1][j-1]) == target {
-				fellas++
-			}
-			//upright
-			if j < len(line)-1 && i > 0 && string(seats[i-1][j+1]) == target {
-				fellas++
-			}
-			//dleft
-			if j > 0 && i < len(seats)-1 && string(seats[i+1][j-1]) == target {
-				fellas++
-			}
-			//dright
-			if j < len(line)-1 && i < len(seats)-1 && string(seats[i+1][j+1]) == target {
-				fellas++
-			}
+			fellas+= march(i,j,seats, "L")
+			fellas+= march(i,j,seats, "UL")
+			fellas+= march(i,j,seats, "U")
+			fellas+= march(i,j,seats, "UR")
+			fellas+= march(i,j,seats, "R")
+			fellas+= march(i,j,seats, "DR")
+			fellas+= march(i,j,seats, "D")
+			fellas+= march(i,j,seats, "DL")
+			
 			if c == "#" {
-				if fellas >= 4 {
+				if fellas >= 5 {
 					nl += "L"
 				} else {
 					nl += "#"
@@ -82,28 +140,27 @@ func applyRules(seats []string) []string {
 }
 
 func eq(a, b []string) bool {
-    if (a == nil) != (b == nil) { 
-        return false; 
-    }
+	if (a == nil) != (b == nil) {
+		return false
+	}
 
-    if len(a) != len(b) {
-        return false
-    }
+	if len(a) != len(b) {
+		return false
+	}
 
-    for i := range a {
-        if a[i] != b[i] {
-            return false
-        }
-    }
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
 
-    return true
+	return true
 }
-
 
 func occupied(seats []string) int {
 	sum := 0
 	for _, l := range seats {
-		sum+= len(l)-len(strings.ReplaceAll(l, "#", ""))
+		sum += len(l) - len(strings.ReplaceAll(l, "#", ""))
 	}
 	return sum
 }
